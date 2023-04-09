eddd991feb565469a774006819e7f2ce95b6b2c8
Ajah Emmanuel Amarachi <emmanuel.fmrs.marach@gmail.com>
2023-04-09 02:42:49 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main - This will print arguments received
- * @argc: no of arguments received
- * @argv: the array itself
+ * main - prints the argument id has received
+ * @argc: number of arguments
+ * @argv: The array of arguments
  * Return: 0 for Success
  */
 
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[1]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
